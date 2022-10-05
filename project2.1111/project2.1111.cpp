d7e64fe9fb5dacfa006cb301a9de967727dc4478
Sofia <kulchitskasofia@gmail.com>
2022-10-05 12:35:27 +0300

    first branch_02 commit

--- project2.1111/project2.1111.cpp
+++ project2.1111/project2.1111.cpp
@@ -11,17 +11,17 @@ using namespace std;
 int main()
 {
 	double a;//вхідний параметр
-	double z1;//результат обчислення 1-го виразу
+	//double z1;//результат обчислення 1-го виразу
 	double z2;//результат обчислення 2-го виразу
 	const double PI = 3.14159265358979;
 
 	cout << "a = "; cin >> a;
 
-	z1 = pow(cos(3 * 1.0 / 8 * PI - a / 4), 2) - pow(cos(11.0 / 8 * PI + a / 4), 2);
+	//z1 = pow(cos(3 * 1.0 / 8 * PI - a / 4), 2) - pow(cos(11.0 / 8 * PI + a / 4), 2);
 	z2 = ((sqrt(2) / 2) * sin(a / 2));
 	cout << endl;
 
-	cout << "z1= " << z1 << endl;
+	//cout << "z1= " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
