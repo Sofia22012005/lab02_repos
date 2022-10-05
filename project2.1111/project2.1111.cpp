7d4bea31f7c4db14f7e27dd77a37f708b4d287d4
Sofia <kulchitskasofia@gmail.com>
2022-10-05 12:23:23 +0300

    first master commit

--- project2.1111/project2.1111.cpp
+++ project2.1111/project2.1111.cpp
@@ -1,22 +1,27 @@
-﻿#include <iostream>
+﻿// Lab_02.cpp
+// < Кульчицька Софія>
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 7
+#include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-	double a;
-	//double z1;
-	double z2;
+	double a;//вхідний параметр
+	double z1;//результат обчислення 1-го виразу
+	double z2;//результат обчислення 2-го виразу
 	const double PI = 3.14159265358979;
 
 	cout << "a = "; cin >> a;
 
-	//z1 = pow(cos(3 * 1.0 / 8 * PI - a / 4), 2) - pow(cos(11.0 / 8 * PI + a / 4), 2);
+	z1 = pow(cos(3 * 1.0 / 8 * PI - a / 4), 2) - pow(cos(11.0 / 8 * PI + a / 4), 2);
 	z2 = ((sqrt(2) / 2) * sin(a / 2));
 	cout << endl;
 
-	//cout << "z1= " << z1 << endl;
+	cout << "z1= " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
